cac2308067e683b4edea7a3f64465c2a66649ed0
newplayer <luckyT320alter@gmail.com>
2021-03-11 21:52:45 +0100

    Added inventory items
    Added food, weapon, shield and chest armor + basic inventory
    script and scene (inventory isn't functional yet).

---
+++ Inventory/Item.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name Item
+
+export(String) var name = ""
+export(Texture) var texture 
+
+export(int) var heal = 0 #stat for foods to heal player
+export(int) var dmg = 0  #stat for weapons
+export(int) var durability = 0 #stat for armor and shield
