51494f14184d8f0a8bfef5a50570d48e601f5fb3
Luywer <luisafonsoso@gmail.com>
2021-03-21 16:20:09 +0000

    inventory ui toggleable

---
+++ InventoryUI.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+onready var inventoryContainer = $InventoryContainer
+onready var button = $TextureButton
+
+func _on_TextureButton_pressed():
+	toggle_inventory()
+
+func _input(event):
+	if event.is_action_pressed("inventory"):
+		toggle_inventory()
+
+func toggle_inventory():
+	inventoryContainer.visible = !inventoryContainer.visible
+	button.release_focus()
