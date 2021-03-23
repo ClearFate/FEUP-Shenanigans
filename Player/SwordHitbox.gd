b4a957b0d0823cce3282a8b223f7375339a9d721
newplayer <luckyT320alter@gmail.com>
2021-03-23 14:31:06 +0100

    Now weapons are working properly

--- Player/SwordHitbox.gd
+++ Player/SwordHitbox.gd
@@ -2,11 +2,14 @@ extends "res://HBoxes/Hitbox.gd"
 
 
 var knockback_vector = Vector2.ZERO
+var stats = PlayerStats
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	stats.connect("damage_changed", self, "update_damage")
 
+func update_damage(val):
+	damage = val
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
