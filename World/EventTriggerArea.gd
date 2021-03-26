c4f6b60cb797061c4be30aba55b40d647751e9ab
Luywer <luisafonsoso@gmail.com>
2021-03-26 20:31:33 +0000

    Added event trigger area

---
+++ World/EventTriggerArea.gd
@@ -0,0 +1,15 @@
+extends "res://HBoxes/InteractableBox.gd"
+
+export(bool) var set_delayed_flags = false
+
+func _ready():
+	can_interact = false;
+
+
+
+
+func _on_EventTriggerArea_body_entered(body):
+	if set_delayed_flags:
+		pass
+	else:
+		interact()
